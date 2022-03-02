83ae1e7dddcb83715a40d020c041d8f7f5a0fbd0
Denis Igorevich <bigdenbstu@yandex.by>
2022-03-02 20:54:26 +0300

    Add methods for work with DB

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -2,14 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/gh0st3e/BillyBetProject/internal/config"
 	"github.com/gh0st3e/BillyBetProject/internal/entity"
 	"github.com/gh0st3e/BillyBetProject/internal/repository"
-	"log"
 )
 
-func main() {
+func AddUser(repos repository.Repositories) {
 
+	err := repos.User.Add(entity.User{
+		Name:    "Denis",
+		Surname: "Leonov",
+		CashID:  2,
+		Ban:     false,
+	})
+	if err != nil {
+		log.Fatalf("cmd.Main.AddUser couldn't add new user, %s", err)
+	}
+}
+
+func AddCash(repos repository.Repositories) {
+
+	err := repos.Cash.Add(entity.Cash{
+		USD: 3,
+		RUB: 300,
+	})
+	if err != nil {
+		log.Fatalf("cmd.Main.AddCash couldn't add new cash, %s", err)
+	}
+}
+
+func GetUser(repos repository.Repositories) {
+	get, err := repos.User.Get(1)
+	if err != nil {
+		log.Fatalf("cmd.Main.GetUser couldn't get user, %s", err)
+	}
+	fmt.Println(get)
+}
+func GetCash(repos repository.Repositories) {
+	get, err := repos.Cash.Get(2)
+	if err != nil {
+		log.Fatalf("cmd.Main.GetCash couldn't get cash, %s", err)
+	}
+	fmt.Println(get)
+}
+func SubCash(repos repository.Repositories) {
+	get, err := repos.Cash.Sub(2, 300)
+	if err != nil {
+		log.Fatalf("cmd.Main.GetCash couldn't sub cash, %s", err)
+	}
+	fmt.Println(get)
+}
+func RemoveUser(repos repository.Repositories) {
+	err := repos.User.Remove(2)
+	if err != nil {
+		log.Fatalf("cmd.Main.RemoveUser couldn't delete user, %s", err)
+	}
+}
+
+func main() {
 	cfg, err := config.Init()
 	if err != nil {
 		log.Fatalf("cmd.Main.Init couldn't init config: %s", err)
@@ -21,18 +73,15 @@ func main() {
 	}
 
 	repos := repository.NewRepositories(db)
-	err = repos.User.Add(entity.User{
-		Name:    "Denis",
-		Surname: "Leonov",
-		CashID:  2,
-		Ban:     false,
-	})
-	if err != nil {
-		log.Fatalf("cmd.Main.Add couldn't add new user, %s", err)
-	}
-
+	//TODO Change arguments!!!
+	//AddCash(repos)
+	//GetCash(repos)
+	//SubCash(repos)
+	//AddUser(repos)
+	//GetUser(repos)
+	//RemoveUser(repos)
 	log.Println(cfg)
 	log.Println(db)
 	log.Println(repos)
-	//db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:8597)/golang")
+
 }
